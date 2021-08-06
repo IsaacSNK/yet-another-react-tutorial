0a190e2eec88af35d2eeb1e940bc5838589c4609
Isaac Ramirez Herrera <joseisaacr@microsoft.com>
2021-08-06 17:35:37 -0600

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import logo from './logo.svg';
+
+export class App extends React.Component {
+  constructor()  {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+  
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const newCounter = this.state.counter + 1;
+      this.setState({
+        counter: newCounter
+      })
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello World!
+          </p>
+          { this.state.counter }
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
